aceb53b0d14b3fb5e48d4fdd8bc76921a331a147
Michał Porażko <121014407+MichalPorazko@users.noreply.github.com>
2024-02-04 23:45:51 +0100

    First steps of the BookStroe App
    First steps of the BookStroe App

--- src/main/scala/Book.scala
+++ src/main/scala/Book.scala
@@ -1,20 +1,32 @@
 case class BookDetails(genre: String, pageCount: Int)
 
-class Book(title: String, author: String, year: Int, bookDetails: BookDetails) {
-  def loadBookDetails(): BookDetails =
-    BookDetails("Fiction", 320)
+case class Publisher(name: String, year:Int, publisherInfo: PublisherInfo){
+  def getPublishername: String = publisherInfo.name
+  def getPublisherIsModern: Boolean = publisherInfo.isModern
 }
 
-object Book
+object Publisher {
 
-  //zastanawiasm się nad taką rzcze no bo w przykladzie w ksiazce jest ze zwracamy (double, double) chat podaje z kolei
-  //że zwracamy (String, Double), a co jeśli klasa ma wiele różnych typów zmiennych a my potrzbujemy różnie raz jednych typów raz innych
-  // co wtedy??
-  // np. w jednym patern matchingu potrzbujemy dwóch String i Int
-  // a w innej Double i String o i co???
+  def unapply(publisher: Publisher): Option[(String, Boolean)] =
+    Some(publisher.publisherInfo.name, publisher.publisherInfo.isModern)
+}
+
+case class PublisherInfo(name: String, isModern: Boolean)
+
+class Book(title: String, author: String, year: Int, private val bookDetails: BookDetails) {
+
+  //I created those two methods to sustain the principles of privacy
+  // by not exposing the Book instance directly but rather the specific information needed for the analyse
+  def getGenre: String = bookDetails.genre
+  def getPageCount: Int = bookDetails.pageCount
+
+}
+
+object Book {
+
   def unapply(book: Book): Option[(String, Int)] =
-    val bookDetails = book.loadBookDetails()
-    Some((bookDetails.genre, bookDetails.pageCount))
+    Some(book.bookDetails.genre, book.bookDetails.pageCount)
+}
 
 sealed trait Category
 case object Fiction extends Category
@@ -25,12 +37,56 @@ case object Long extends Category
 val fictionGenre = "Fiction"
 val pageCountThreshold: Int = 300
 
+
+
 //not using the unapply method
+def categorize(book: Book): Set[Category] =
+  val genre = book.getGenre
+  val pageCount = book.getPageCount
+  (genre, pageCount) match
+    case (fiction, pageCount) if pageCount >= pageCountThreshold && fiction == fictionGenre => Set(Fiction, Long)
+    case (_, pageCount) if pageCount >= pageCountThreshold => Set(Long)
+    case (fiction, _) if fiction == fictionGenre => Set(Fiction)
+    case _ => Set(NonFiction, Short)
+
+
+//using the unapply method
 def analyse(book: Book): Set[Category] =
-  book.loadBookDetails() match
+  book match
     case Book(fiction, pageCount) if pageCount>= pageCountThreshold && fiction == fictionGenre => Set(Fiction, Long)
     case Book(_, pageCount) if pageCount>= pageCountThreshold => Set(Long)
+    case Book(fiction, _) if fiction == fictionGenre => Set(Fiction)
+    case _ => Set(NonFiction, Short)
 
+def analyseSecondVersion(book: Book): Set[Category] = book match {
+  case Book(`fictionGenre`, pageCount, _, isModern) if pageCount >= pageCountThreshold && isModern => Set(Fiction, Long, ModernPublisher)
+  case Book(`fictionGenre`, _, _, _) => Set(Fiction)
+  case Book(_, pageCount, _, isModern) if pageCount >= pageCountThreshold && isModern => Set(Long, ModernPublisher)
+  case Book(_, _, _, isModern) if isModern => Set(ModernPublisher)
+  case _ => Set(NonFiction, Short)
+}
+
+
+val books = List(
+  new Book("1984", "George Orwell", 1949, BookDetails("Dystopian", 328)),
+  new Book("To Kill a Mockingbird", "Harper Lee", 1960, BookDetails("Fiction", 281)),
+  new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, BookDetails("Fiction", 180)),
+  new Book("A Brief History of Time", "Stephen Hawking", 1988, BookDetails("Non-Fiction", 212))
+)
+
+
+
+object MainBook {
+
+
+  def main(args: Array[String]): Unit = {
+    books.foreach{ book =>
+      val bookDetails = analyse(book)
+      println(bookDetails)
+    }
+
+  }
+}
 
 
 
